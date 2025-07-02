adc1a446d0c473fb78acb412be20c7493782adcf
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-07-02 09:01:48 -0500

    refactor bim360 services to have one fetch function to streamline error handling

--- src/pages/services/bim360.services.js
+++ src/pages/services/bim360.services.js
@@ -2,148 +2,49 @@ const backendUrl = import.meta.env.VITE_API_BACKEND_BASE_URL;
 
 import { toBase64 } from "../utils/base64.util";
 
-export const fetchBim360ProjectsData = async () => {
+async function fetchBim360(url, fetchLabel){
   try {
-    const response = await fetch(`${backendUrl}/bim360/projects`, {
+    const response = await fetch(url, {
       method: "GET",
       credentials: "include",
     });
 
     if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 projects");
+      throw new Error(`Failed to fetch BIM 360 ${fetchLabel}`);
     }
 
     const { data } = await response.json();
-    console.debug("Fetched BIM 360 projects:", data);
+    console.debug(`Fetched BIM 360 ${fetchLabel}:`, data);
 
     return data;
   } catch (error) {
-    console.error("Error fetching BIM 360 projects:", error);
+    console.error(`Error fetching BIM 360 ${fetchLabel}:`, error);
     throw error;
   }
+
+}
+
+export const fetchBim360ProjectsData = async () => {
+  return await fetchBim360(`${backendUrl}/bim360/projects`,"projects")
 };
 
 export const fetchBim360ProjectData = async (projectId, accountId) => {
-  try {
-    const response = await fetch(
-      `${backendUrl}/bim360/projects/${accountId}/${projectId}`,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 project data");
-    }
-
-    const { data } = await response.json();
-
-    console.log("Fetched BIM 360 project data:", data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching BIM 360 project data:", error);
-    throw error;
-  }
+  return await fetchBim360(`${backendUrl}/bim360/projects/${accountId}/${projectId}`, "project data")
 };
 
 export const fetchBim360FederatedModel = async (projectId, accountId) => {
-  try {
-    const response = await fetch(
-      `${backendUrl}/datamanagement/${accountId}/${projectId}/federated-model`,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 federated model");
-    }
-
-    const { data } = await response.json();
-
-    console.debug("Fetched BIM 360 federated model:", data);
-
-    return data.federatedmodel ? await toBase64(data.federatedmodel) : null;
-  } catch (error) {
-    console.error("Error fetching BIM 360 federated model:", error);
-    throw error;
-  }
+  const data = await fetchBim360(`${backendUrl}/datamanagement/${accountId}/${projectId}/federated-model`,"federated model")
+  return data.federatedModel ? await toBase64(data.federatedModel) : null
 };
 
 export const fetchBim360ProjectUsers = async (projectId, accountId) => {
-  try {
-    const response = await fetch(
-      `${backendUrl}/bim360/projects/${accountId}/${projectId}/users`,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 project users");
-    }
-
-    const { data } = await response.json();
-
-    console.debug("Fetched BIM 360 project users:", data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching BIM 360 project users:", error);
-    throw error;
-  }
+  return await fetchBim360(`${backendUrl}/bim360/projects/${accountId}/${projectId}/users`, "project users")
 };
 
 export const fetchBim360ProjectIssues = async (projectId, accountId) => {
-  try {
-    const response = await fetch(
-      `${backendUrl}/bim360/projects/${accountId}/${projectId}/issues`,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 project issues");
-    }
-
-    const { data } = await response.json();
-
-    console.debug("Fetched BIM 360 project issues:", data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching BIM 360 project issues:", error);
-    throw error;
-  }
+  return await fetchBim360(`${backendUrl}/bim360/projects/${accountId}/${projectId}/issues`, "project issues")
 };
 
 export const fetchBim360ProjectRfis = async (projectId, accountId) => {
-  try {
-    const response = await fetch(
-      `${backendUrl}/bim360/projects/${accountId}/${projectId}/rfis`,
-      {
-        method: "GET",
-        credentials: "include",
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch BIM 360 project RFIs");
-    }
-
-    const { data } = await response.json();
-
-    console.debug("Fetched BIM 360 project RFIs:", data);
-
-    return data;
-  } catch (error) {
-    console.error("Error fetching BIM 360 project RFIs:", error);
-    throw error;
-  }
+  return await fetchBim360(`${backendUrl}/bim360/projects/${accountId}/${projectId}/rfis`, "project RFIs")
 };
