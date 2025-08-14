fc2d525ce6ced024c8fc248a5eec2506d8b7ea4a
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-08-14 11:36:46 -0500

    add private route; formatting

--- src/App.jsx
+++ src/App.jsx
@@ -22,93 +22,105 @@ import Bim360ProjectUsersPage from "./pages/bim360/bim360.users.page.jsx";
 import Bim360ProjectRfisPage from "./pages/bim360/bim360.rfis.page.jsx";
 import Bim360ProjectFolderPermitsPage from "./pages/bim360/bim360.folder.permits.page.jsx";
 import Bim360ModelDatabasePage from "./pages/bim360/bim360.model.database.page";
-import Bim360DigitalTwin from "./pages/bim360/bim360.digital.twin.page"
+import Bim360DigitalTwin from "./pages/bim360/bim360.digital.twin.page";
 
-import UnauthorizedPage from "./pages/general/UnaurhorizedPage.jsx";
+import UnauthorizedPage from "./pages/general/Unauthorized.page.jsx";
 import NotFoundPage from "./pages/general/NotFoundPage";
+import PrivateRoutes from "./utils/PrivateRoutes.jsx";
 
 function App() {
-  return (
-    <CookiesProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/platform" element={<PlatformPage />} />
+    return (
+        <CookiesProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route element={<PrivateRoutes />}>
+                        <Route path="/platform" element={<PlatformPage />} />
 
-          {/* ACC Pages */}
-          <Route path="/acc/projects" element={<AccProjectsPage />} />
-          <Route
-            path="/acc/projects/:accountId/:projectId"
-            element={<AccProjectPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/issues"
-            element={<AccProjectIssuesPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/users"
-            element={<AccProjectUsersPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/rfis"
-            element={<AccProjectRfisPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/submittals"
-            element={<AccProjectSubmittalsPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/folder-permits"
-            element={<AccProjectFolderPermitsPage />}
-          />
-          <Route
-            path="/acc/projects/:accountId/:projectId/model-database"
-            element={<AccModelDatabasePage />}
-          />
-          <Route 
-            path = "/acc/projects/:accountId/:projectId/digital-twin"
-            element = {<AccDigitalTwin />}
-            />
+                        {/* ACC Pages */}
+                        <Route
+                            path="/acc/projects"
+                            element={<AccProjectsPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId"
+                            element={<AccProjectPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/issues"
+                            element={<AccProjectIssuesPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/users"
+                            element={<AccProjectUsersPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/rfis"
+                            element={<AccProjectRfisPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/submittals"
+                            element={<AccProjectSubmittalsPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/folder-permits"
+                            element={<AccProjectFolderPermitsPage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/model-database"
+                            element={<AccModelDatabasePage />}
+                        />
+                        <Route
+                            path="/acc/projects/:accountId/:projectId/digital-twin"
+                            element={<AccDigitalTwin />}
+                        />
 
-          {/* BIM 360 */}
-          <Route path="/bim360/projects/" element={<Bim360ProjectsPage />} />
-          <Route
-            path="/bim360/projects/:accountId/:projectId"
-            element={<Bim360ProjectPage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/issues"
-            element={<Bim360ProjectIssuesPage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/users"
-            element={<Bim360ProjectUsersPage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/rfis"
-            element={<Bim360ProjectRfisPage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/folder-permits"
-            element={<Bim360ProjectFolderPermitsPage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/model-database"
-            element={<Bim360ModelDatabasePage />}
-          />
-          <Route
-            path="/bim360/projects/:accountId/:projectId/digital-twin"
-            element = {<Bim360DigitalTwin/>}
-            />
+                        {/* BIM 360 */}
+                        <Route
+                            path="/bim360/projects/"
+                            element={<Bim360ProjectsPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId"
+                            element={<Bim360ProjectPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/issues"
+                            element={<Bim360ProjectIssuesPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/users"
+                            element={<Bim360ProjectUsersPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/rfis"
+                            element={<Bim360ProjectRfisPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/folder-permits"
+                            element={<Bim360ProjectFolderPermitsPage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/model-database"
+                            element={<Bim360ModelDatabasePage />}
+                        />
+                        <Route
+                            path="/bim360/projects/:accountId/:projectId/digital-twin"
+                            element={<Bim360DigitalTwin />}
+                        />
+                    </Route>
 
-          {/* Catch-all route for 404 */}
-          <Route path="/unauthorized" element={<UnauthorizedPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Router>
-    </CookiesProvider>
-  );
+                    {/* Catch-all route for 404 */}
+                    <Route
+                        path="/unauthorized"
+                        element={<UnauthorizedPage />}
+                    />
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </Router>
+        </CookiesProvider>
+    );
 }
 
 export default App;
