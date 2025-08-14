cdf04e2004e5b523e56bf973d91f1021d11f2fcb
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-08-14 11:34:47 -0500

    refactor to make test fixture own file

---
+++ tests/fixtures/test.fixture.js
@@ -0,0 +1,15 @@
+import { test as base } from "@playwright/test";
+import { mockUserProfileAPI, loginUser } from "./auth.fixture";
+
+// extends playwright test
+export const test = base.extend({
+    loggedInPage: async ({ page }, use) => {
+        await mockUserProfileAPI(page);
+
+        // Log in before running the test
+        await loginUser(page);
+
+        // Use the authenticated page in the test
+        await use(page);
+    },
+});
