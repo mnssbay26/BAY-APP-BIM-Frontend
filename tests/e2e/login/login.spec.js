ec92da7fb81dde4f47660cc6ebbfe6a0a28fa511
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-08-14 11:27:41 -0500

    formatting; use of loggedInPage for test

--- tests/e2e/login/login.spec.js
+++ tests/e2e/login/login.spec.js
@@ -1,58 +1,60 @@
 // tests/e2e/authentication/login.spec.js
-import { test, expect } from '@playwright/test';
-import LoginPage from '../../pages/general/Login.page';
+import { test } from "../../fixtures/test.fixture";
+import { expect } from "@playwright/test";
+import LoginPage from "../../pages/general/Login.page";
 
-test.describe('Login Page - Logged Out', () => {
-  test('should display login button when not logged in', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    
-    await loginPage.navigate();
-    await expect(loginPage.isLoaded()).resolves.toBeTruthy();
-    
-    // Verify title and subtitle
-    const title = await loginPage.getTitle();
-    expect(title).toBe('BAYER BIM APP');
-    
-    const subtitle = await loginPage.getSubtitle();
-    expect(subtitle).toContain('Login with Autodesk');
-    
-    // Verify login button is visible
-    expect(await loginPage.isLoginButtonVisible()).toBeTruthy();
-    expect(await loginPage.isLogoutButtonVisible()).toBeFalsy();
-  });
+test.describe("Login Page - Logged Out", () => {
+    test("should display login button when not logged in", async ({ page }) => {
+        const loginPage = new LoginPage(page);
 
-  test('should redirect to Autodesk OAuth when login button is clicked', async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    
-    await loginPage.navigate();
-    await loginPage.clickLoginWithAutodesk()
-    
-    
-    const currentUrl = await loginPage.getCurrentUrl()
-    expect(currentUrl).toContain('autodesk.com');
-  });
+        await loginPage.navigate();
+        await expect(loginPage.isLoaded()).resolves.toBeTruthy();
+
+        // Verify title and subtitle
+        const title = await loginPage.getTitle();
+        expect(title).toBe("BAYER BIM APP");
+
+        const subtitle = await loginPage.getSubtitle();
+        expect(subtitle).toContain("Login with Autodesk");
+
+        // Verify login button is visible
+        expect(await loginPage.isLoginButtonVisible()).toBeTruthy();
+        expect(await loginPage.isLogoutButtonVisible()).toBeFalsy();
+    });
+
+    test("should redirect to Autodesk OAuth when login button is clicked", async ({
+        page,
+    }) => {
+        const loginPage = new LoginPage(page);
+
+        await loginPage.navigate();
+        await loginPage.clickLoginWithAutodesk();
+
+        const currentUrl = await loginPage.getCurrentUrl();
+        expect(currentUrl).toContain("autodesk.com");
+    });
 });
 
 test.describe("Login page - Logged in", () => {
-    test("should be able to get /login even if logged in", async({page}) => {
-        const loginPage = new LoginPage(page)
+    test("should be able to get /login even if logged in", async ({
+        loggedInPage,
+    }) => {
+        const loginPage = new LoginPage(loggedInPage);
+        await loginPage.navigate();
 
-        await loginPage.mockLoggedInState()
+        const currentUrl = await loginPage.getCurrentUrl();
+        expect(currentUrl).toContain("/login");
+    });
+    test("should see different elements on logged in", async ({
+        loggedInPage,
+    }) => {
+        const loginPage = new LoginPage(loggedInPage);
 
-        const currentUrl = await loginPage.getCurrentUrl()
-        expect(currentUrl).toContain("/login")
-    })
-    test("should see different elements on logged in", async({page}) => {
-        const loginPage = new LoginPage(page)
+        await loginPage.navigate();
+        const logoutVisible = await loginPage.isLogoutButtonVisible();
+        expect(logoutVisible).toBeTruthy();
 
-        await loginPage.mockLoggedInState()
-
-        const logoutVisible = await loginPage.isLogoutButtonVisible()
-        expect(logoutVisible).toBeTruthy()
-
-        const title = await loginPage.getSubtitle()
-        expect(title).toContain("Welcome back")
-
-
-    })
-})
+        const title = await loginPage.getSubtitle();
+        expect(title).toContain("Welcome back");
+    });
+});
