b88c137f5097e00bc3bdb415d72f50eb36032406
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-08-14 11:37:14 -0500

    formatting; fix bug where redirect before load

--- src/utils/PrivateRoutes.jsx
+++ src/utils/PrivateRoutes.jsx
@@ -1,13 +1,20 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useUserProfile } from "@/hooks/useUserProfile";
 import BayerLoadingOverlay from "@/components/general/general.pages.loading";
-import sleep from "./sleep";
 
 export default function PrivateRoutes() {
-    const { userProfile, isLoading } = useUserProfile();
+    const { userProfile, isLoading, error } = useUserProfile();
+
+    // if something goes wrong, go to login
+    if (error) {
+        return <Navigate to="/login" replace />;
+    }
+
+    // if loading, show it
     if (isLoading || userProfile === undefined) {
         return <BayerLoadingOverlay />;
     }
-    /// return (userProfile ? <div>{userProfile}</div> : <div>{userProfile}</div>)
+
+    // on logged in, show Route, else navigate to Login
     return userProfile ? <Outlet /> : <Navigate to="/login" replace />;
 }
