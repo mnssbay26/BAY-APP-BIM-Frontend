aafbaeb7d3a6934c0c4341f2ad5af44507624337
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-09-08 13:38:36 -0500

    formatting; update header imports

--- src/components/platform_general_components/acc_components/acc.main.layout.jsx
+++ src/components/platform_general_components/acc_components/acc.main.layout.jsx
@@ -1,24 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import Acc360ProjectsHeader from '../../platform_general_components/acc_components/acc.platform.projects.header'; 
-import BayerAccSidebar from './acc.platform.sidebar.jsx'; 
-import BayerFooter from '../../general/general.pages.footer.jsx'; 
+import Acc360ProjectsHeader from "../../headers/acc.platform.projects.header";
+import BayerAccSidebar from "./acc.platform.sidebar.jsx";
+import BayerFooter from "../../general/general.pages.footer.jsx";
 
 const BayerAccMainLayout = ({ children, projectId, accountId }) => {
-
-  
-  return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <Acc360ProjectsHeader projectId={projectId} accountId={accountId}/>
-      <div className="flex flex-1 pt-16">
-        <BayerAccSidebar />
-        <main className="flex-1 p-3 overflow-y-auto">
-          {children}
-        </main>
-      </div>
-      <BayerFooter />
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen bg-white">
+            <Acc360ProjectsHeader projectId={projectId} accountId={accountId} />
+            <div className="flex flex-1 pt-16">
+                <BayerAccSidebar />
+                <main className="flex-1 p-3 overflow-y-auto">{children}</main>
+            </div>
+            <BayerFooter />
+        </div>
+    );
 };
 
 export default BayerAccMainLayout;
