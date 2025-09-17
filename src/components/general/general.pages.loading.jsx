6f3cf4dfcb12f3eb0b3e77285e4e5a5f88add30c
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-09-17 12:01:58 -0500

    formatting

--- src/components/general/general.pages.loading.jsx
+++ src/components/general/general.pages.loading.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 export default function BayerLoadingOverlay({ message }) {
-  return (
-    <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white/80">
-      {/* Bayer logo: absolute path from public */}
-      <img
-        src="/bayerLoading.svg"
-        alt="Bayer Logo"
-        className="h-17 mb-1"
-      />
+    return (
+        <div className="fixed inset-0 z-50 flex flex-col items-center justify-center bg-white/80">
+            {/* Bayer logo: absolute path from public */}
+            <img
+                src="/bayerLoading.svg"
+                alt="Bayer Logo"
+                className="h-17 mb-1"
+            />
 
-      {/* Loading message */}
-      <p className="text-gray-700 text-lg">{message}</p>
-    </div>
-  );
+            {/* Loading message */}
+            <p className="text-gray-700 text-lg">{message}</p>
+        </div>
+    );
 }
