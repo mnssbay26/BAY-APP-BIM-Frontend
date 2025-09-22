452dfc76309cc5ad1b9ff8b1105e9764fcafd6c2
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-09-22 10:13:08 -0500

    fix header left display based on if platform initialized

--- src/components/headers/general-header.jsx
+++ src/components/headers/general-header.jsx
@@ -1,4 +1,5 @@
 // seeks to combine general.pages.header and platform.access.header
+import { useSelector } from "react-redux";
 import { useUserSession } from "@/hooks/useUserSession";
 import NoPlatformHeaderLeft from "./left_headers/no-platform-header-left";
 import LoggedInHeaderRight from "./right_headers/logged-in-header-right";
@@ -7,13 +8,20 @@ import PlatformHeaderLeft from "./left_headers/platform-header-left";
 
 export default function GeneralHeader() {
     const { userProfile } = useUserSession();
+    const isAcc = useSelector((store) => store.projects.isAcc);
+    const platformSelected = isAcc !== null;
 
     return (
         <header
             className="fixed top-0 left-0 z-50 w-full h-16 flex items-center justify-between px-6 bg-white border-b shadow"
             role="banner"
+            data-testid="new-header"
         >
-            {userProfile ? <NoPlatformHeaderLeft /> : <PlatformHeaderLeft />}
+            {platformSelected ? (
+                <PlatformHeaderLeft />
+            ) : (
+                <NoPlatformHeaderLeft />
+            )}
             {userProfile ? (
                 <LoggedInHeaderRight userProfile={userProfile} />
             ) : (
