2efc0ea6cf95c8fafa975366785e5c8c56253e27
James 'Trey' Williams <james.williams1.ext@bayer.com>
2025-09-22 10:10:36 -0500

    formatting; add data-testid

--- src/components/headers/left_headers/no-platform-header-left.jsx
+++ src/components/headers/left_headers/no-platform-header-left.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 export default function NoPlatformHeaderLeft() {
     return (
-        <div className="flex items-center space-x-4">
+        <div
+            className="flex items-center space-x-4"
+            data-testid="no-platform-header-left"
+        >
             <Link
                 to="/"
                 className="text-lg font-bold text-gray-800 hover:text-blue-600 transition-colors"
